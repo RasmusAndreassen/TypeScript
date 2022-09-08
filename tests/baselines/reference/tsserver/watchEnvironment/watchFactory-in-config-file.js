2ecc04c3a02e68b0a06824ab659f99ec906fb3aa
Sheetal Nandi <shkamat@microsoft.com>
2022-09-08 10:43:56 -0700

    Dont print unnecessary plugin host not implemented msg in logs

--- tests/baselines/reference/tsserver/watchEnvironment/watchFactory-in-config-file.js
+++ tests/baselines/reference/tsserver/watchEnvironment/watchFactory-in-config-file.js
@@ -63,15 +63,14 @@ Info 7    [00:00:30.000] FileWatcher:: Close:: WatchInfo: /user/username/project
 Info 8    [00:00:31.000] FileWatcher:: Added:: WatchInfo: /user/username/projects/myproject/tsconfig.json 2000 {"watchFactory":"myplugin"} Project: /user/username/projects/myproject/tsconfig.json WatchType: Config file
 Info 9    [00:00:32.000] DirectoryWatcher:: Added:: WatchInfo: /user/username/projects/myproject 1 {"watchFactory":"myplugin"} Config: /user/username/projects/myproject/tsconfig.json WatchType: Wild card directory
 Info 10   [00:00:33.000] Elapsed:: *ms DirectoryWatcher:: Added:: WatchInfo: /user/username/projects/myproject 1 {"watchFactory":"myplugin"} Config: /user/username/projects/myproject/tsconfig.json WatchType: Wild card directory
-Info 11   [00:00:34.000] Plugins were requested but not running in environment that supports 'require'. Nothing will be loaded
-Info 12   [00:00:35.000] FileWatcher:: Added:: WatchInfo: /user/username/projects/myproject/b.ts 500 undefined WatchType: Closed Script info
-Info 13   [00:00:36.000] Starting updateGraphWorker: Project: /user/username/projects/myproject/tsconfig.json
-Info 14   [00:00:37.000] FileWatcher:: Added:: WatchInfo: /a/lib/lib.d.ts 500 undefined WatchType: Closed Script info
-Info 15   [00:00:38.000] DirectoryWatcher:: Added:: WatchInfo: /user/username/projects/myproject/node_modules/@types 1 {"watchFactory":"myplugin"} Project: /user/username/projects/myproject/tsconfig.json WatchType: Type roots
-Info 16   [00:00:39.000] Elapsed:: *ms DirectoryWatcher:: Added:: WatchInfo: /user/username/projects/myproject/node_modules/@types 1 {"watchFactory":"myplugin"} Project: /user/username/projects/myproject/tsconfig.json WatchType: Type roots
-Info 17   [00:00:40.000] Finishing updateGraphWorker: Project: /user/username/projects/myproject/tsconfig.json Version: 1 structureChanged: true structureIsReused:: Not Elapsed:: *ms
-Info 18   [00:00:41.000] Project '/user/username/projects/myproject/tsconfig.json' (Configured)
-Info 19   [00:00:42.000] 	Files (3)
+Info 11   [00:00:34.000] FileWatcher:: Added:: WatchInfo: /user/username/projects/myproject/b.ts 500 undefined WatchType: Closed Script info
+Info 12   [00:00:35.000] Starting updateGraphWorker: Project: /user/username/projects/myproject/tsconfig.json
+Info 13   [00:00:36.000] FileWatcher:: Added:: WatchInfo: /a/lib/lib.d.ts 500 undefined WatchType: Closed Script info
+Info 14   [00:00:37.000] DirectoryWatcher:: Added:: WatchInfo: /user/username/projects/myproject/node_modules/@types 1 {"watchFactory":"myplugin"} Project: /user/username/projects/myproject/tsconfig.json WatchType: Type roots
+Info 15   [00:00:38.000] Elapsed:: *ms DirectoryWatcher:: Added:: WatchInfo: /user/username/projects/myproject/node_modules/@types 1 {"watchFactory":"myplugin"} Project: /user/username/projects/myproject/tsconfig.json WatchType: Type roots
+Info 16   [00:00:39.000] Finishing updateGraphWorker: Project: /user/username/projects/myproject/tsconfig.json Version: 1 structureChanged: true structureIsReused:: Not Elapsed:: *ms
+Info 17   [00:00:40.000] Project '/user/username/projects/myproject/tsconfig.json' (Configured)
+Info 18   [00:00:41.000] 	Files (3)
 	/a/lib/lib.d.ts
 	/user/username/projects/myproject/a.ts
 	/user/username/projects/myproject/b.ts
@@ -84,14 +83,14 @@ Info 19   [00:00:42.000] 	Files (3)
 	b.ts
 	  Matched by default include pattern '**/*'
 
-Info 20   [00:00:43.000] -----------------------------------------------
-Info 21   [00:00:44.000] Project '/user/username/projects/myproject/tsconfig.json' (Configured)
-Info 21   [00:00:45.000] 	Files (3)
+Info 19   [00:00:42.000] -----------------------------------------------
+Info 20   [00:00:43.000] Project '/user/username/projects/myproject/tsconfig.json' (Configured)
+Info 20   [00:00:44.000] 	Files (3)
 
-Info 21   [00:00:46.000] -----------------------------------------------
-Info 21   [00:00:47.000] Open files: 
-Info 21   [00:00:48.000] 	FileName: /user/username/projects/myproject/a.ts ProjectRootPath: undefined
-Info 21   [00:00:49.000] 		Projects: /user/username/projects/myproject/tsconfig.json
+Info 20   [00:00:45.000] -----------------------------------------------
+Info 20   [00:00:46.000] Open files: 
+Info 20   [00:00:47.000] 	FileName: /user/username/projects/myproject/a.ts ProjectRootPath: undefined
+Info 20   [00:00:48.000] 		Projects: /user/username/projects/myproject/tsconfig.json
 After request
 
 PolledWatches::
@@ -115,15 +114,15 @@ Plugin Watches::
 WatchedFiles::
 WatchedDirectories:Recursive::
 WatchedDirectories::
-Info 21   [00:00:50.000] response:
+Info 20   [00:00:49.000] response:
     {
       "responseRequired": false
     }
-Info 22   [00:00:51.000] Change file
-Info 23   [00:00:55.000] FileWatcher:: Triggered with /user/username/projects/myproject/b.ts 1:: WatchInfo: /user/username/projects/myproject/b.ts 500 undefined WatchType: Closed Script info
-Info 24   [00:00:56.000] Scheduled: /user/username/projects/myproject/tsconfig.json
-Info 25   [00:00:57.000] Scheduled: *ensureProjectForOpenFiles*
-Info 26   [00:00:58.000] Elapsed:: *ms FileWatcher:: Triggered with /user/username/projects/myproject/b.ts 1:: WatchInfo: /user/username/projects/myproject/b.ts 500 undefined WatchType: Closed Script info
+Info 21   [00:00:50.000] Change file
+Info 22   [00:00:54.000] FileWatcher:: Triggered with /user/username/projects/myproject/b.ts 1:: WatchInfo: /user/username/projects/myproject/b.ts 500 undefined WatchType: Closed Script info
+Info 23   [00:00:55.000] Scheduled: /user/username/projects/myproject/tsconfig.json
+Info 24   [00:00:56.000] Scheduled: *ensureProjectForOpenFiles*
+Info 25   [00:00:57.000] Elapsed:: *ms FileWatcher:: Triggered with /user/username/projects/myproject/b.ts 1:: WatchInfo: /user/username/projects/myproject/b.ts 500 undefined WatchType: Closed Script info
 Before running timeout callbacks
 //// [/user/username/projects/myproject/b.ts]
 export class a { prop = "hello"; foo() { return this.prop; } }
@@ -150,27 +149,27 @@ Plugin Watches::
 WatchedFiles::
 WatchedDirectories:Recursive::
 WatchedDirectories::
-Info 27   [00:00:59.000] Running: /user/username/projects/myproject/tsconfig.json
-Info 28   [00:01:00.000] Starting updateGraphWorker: Project: /user/username/projects/myproject/tsconfig.json
-Info 29   [00:01:01.000] Finishing updateGraphWorker: Project: /user/username/projects/myproject/tsconfig.json Version: 2 structureChanged: false structureIsReused:: Completely Elapsed:: *ms
-Info 30   [00:01:02.000] Different program with same set of files
-Info 31   [00:01:03.000] Running: *ensureProjectForOpenFiles*
-Info 32   [00:01:04.000] Before ensureProjectForOpenFiles:
-Info 33   [00:01:05.000] Project '/user/username/projects/myproject/tsconfig.json' (Configured)
-Info 33   [00:01:06.000] 	Files (3)
+Info 26   [00:00:58.000] Running: /user/username/projects/myproject/tsconfig.json
+Info 27   [00:00:59.000] Starting updateGraphWorker: Project: /user/username/projects/myproject/tsconfig.json
+Info 28   [00:01:00.000] Finishing updateGraphWorker: Project: /user/username/projects/myproject/tsconfig.json Version: 2 structureChanged: false structureIsReused:: Completely Elapsed:: *ms
+Info 29   [00:01:01.000] Different program with same set of files
+Info 30   [00:01:02.000] Running: *ensureProjectForOpenFiles*
+Info 31   [00:01:03.000] Before ensureProjectForOpenFiles:
+Info 32   [00:01:04.000] Project '/user/username/projects/myproject/tsconfig.json' (Configured)
+Info 32   [00:01:05.000] 	Files (3)
 
-Info 33   [00:01:07.000] -----------------------------------------------
-Info 33   [00:01:08.000] Open files: 
-Info 33   [00:01:09.000] 	FileName: /user/username/projects/myproject/a.ts ProjectRootPath: undefined
-Info 33   [00:01:10.000] 		Projects: /user/username/projects/myproject/tsconfig.json
-Info 33   [00:01:11.000] After ensureProjectForOpenFiles:
-Info 34   [00:01:12.000] Project '/user/username/projects/myproject/tsconfig.json' (Configured)
-Info 34   [00:01:13.000] 	Files (3)
+Info 32   [00:01:06.000] -----------------------------------------------
+Info 32   [00:01:07.000] Open files: 
+Info 32   [00:01:08.000] 	FileName: /user/username/projects/myproject/a.ts ProjectRootPath: undefined
+Info 32   [00:01:09.000] 		Projects: /user/username/projects/myproject/tsconfig.json
+Info 32   [00:01:10.000] After ensureProjectForOpenFiles:
+Info 33   [00:01:11.000] Project '/user/username/projects/myproject/tsconfig.json' (Configured)
+Info 33   [00:01:12.000] 	Files (3)
 
-Info 34   [00:01:14.000] -----------------------------------------------
-Info 34   [00:01:15.000] Open files: 
-Info 34   [00:01:16.000] 	FileName: /user/username/projects/myproject/a.ts ProjectRootPath: undefined
-Info 34   [00:01:17.000] 		Projects: /user/username/projects/myproject/tsconfig.json
+Info 33   [00:01:13.000] -----------------------------------------------
+Info 33   [00:01:14.000] Open files: 
+Info 33   [00:01:15.000] 	FileName: /user/username/projects/myproject/a.ts ProjectRootPath: undefined
+Info 33   [00:01:16.000] 		Projects: /user/username/projects/myproject/tsconfig.json
 After running timeout callbacks
 
 PolledWatches::
